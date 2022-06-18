00473a88cdb56dc3a849da9d7eb6406381c375ab
taka0713 <tyunisu2018@gmail.com>
2022-06-19 01:57:25 +0900

    message

--- script.js
+++ script.js
@@ -1,6 +1,66 @@
 $(function () {
-  $("#button_one").click(function () {
+  $(".phone_button").click(function () {
     let one = $("#number_input").text();
-    $("#number_input").text(one + "1");
+    $(this).text(one + data?1:@;);
+  });
+
+  // $("#button_two").click(function () {
+  //   let one = $("#number_input").text();
+  //   $("#number_input").text(one + "2");
+  // });
+
+  // $("#button_three").click(function () {
+  //   let one = $("#number_input").text();
+  //   $("#number_input").text(one + "3");
+  // });
+
+  // $("#button_four").click(function () {
+  //   let one = $("#number_input").text();
+  //   $("#number_input").text(one + "4");
+  // });
+
+  // $("#button_five").click(function () {
+  //   let one = $("#number_input").text();
+  //   $("#number_input").text(one + "5");
+  // });
+
+  // $("#button_six").click(function () {
+  //   let one = $("#number_input").text();
+  //   $("#number_input").text(one + "6");
+  // });
+
+  // $("#button_seven").click(function () {
+  //   let one = $("#number_input").text();
+  //   $("#number_input").text(one + "7");
+  // });
+
+  // $("#button_eight").click(function () {
+  //   let one = $("#number_input").text();
+  //   $("#number_input").text(one + "8");
+  // });
+
+  // $("#button_nine").click(function () {
+  //   let one = $("#number_input").text();
+  //   $("#number_input").text(one + "9");
+  // });
+
+  // $("#button_ten").click(function () {
+  //   let one = $("#number_input").text();
+  //   $("#number_input").text(one + "*");
+  // });
+
+  // $("#button_zero").click(function () {
+  //   let one = $("#number_input").text();
+  //   $("#number_input").text(one + "0");
+  // });
+
+  // $("#button_eleven").click(function () {
+  //   let one = $("#number_input").text();
+  //   $("#number_input").text(one + "#");
+  // });
+
+  $("#button_call").click(function () {
+    let one = $("#number_input").text();
+    alert(one + "に電話をかけます。");
   });
 });
