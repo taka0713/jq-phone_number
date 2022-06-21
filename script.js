3933f36cad97229417633da04927048deeacc7c3
taka0713 <tyunisu2018@gmail.com>
2022-06-22 02:42:14 +0900

    message

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 $(function () {
-  $(".phone_button").click(function () {
-    $("#number_input").text($("#number_input").text() + $(this).data("id"));
+  $("#button_one").click(function () {
+    let one = $("#number_input").text();
+    $("#number_input").text(one + "1");
+  });
+  $("#button_call").click(function () {
+    let one = $("#number_input").text();
+    alert(one + "に電話をかけます。");
   });
 
-  $("#button_call").click(function () {
-    alert($("#number_input").text() + "に電話をかけます。");
+  $(".phone_button").click(function () {
+    $("#").text("id") !== "@"
+      ? $("#number_input").text()
+      : alert("に電話をかけます。");
   });
 });
-
-// $(".phone_button").click(function () {
-//   $(this).data("id") !== "@"
-//     ? $("#number_input").text($("#number_input").text() + $(this).data("id"))
-//     : alert($("#number_input").text() + "に電話をかけます。");
-// });
