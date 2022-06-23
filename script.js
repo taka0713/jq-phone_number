60161a06e5da5ec39714e100a346544db7525785
taka0713 <tyunisu2018@gmail.com>
2022-06-24 01:49:39 +0900

    message

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 $(function () {
-  // $(".phone_button").click(function () {
-  //   if ($(this).data("id") !== "@") {
-  //     $("#number_input").text($("#number_input").text() + $(this).data("id"));
-  //   } else {
-  //     alert($("#number_input").text() + "に電話をかけます。");
-  //   }
-  // });
-
+  let button_delete;
+  let defaultSet = () => {
+    if ($("#number_input").text() === "") {
+      $(".phone_de").hide();
+    }
+  };
+  defaultSet();
   $(".phone_button").click(function () {
-    $(this).data("id") !== "@"
-      ? $("#number_input").text($("#number_input").text() + $(this).data("id"))
-      : alert($("#number_input").text() + "に電話をかけます。");
+    $(".phone_de").show();
+    if ($(this).data("id") !== "@") {
+      $("#number_input").text($("#number_input").text() + $(this).data("id"));
+    } else {
+      alert($("#number_input").text() + "に電話をかけます。");
+    }
+  });
+  $("#button_twelve").click(function () {
+    $("#number_input").text("");
+    defaultSet();
+  });
+  $("#button_thirty").click(function () {
+    let number_delete = $("#number_input").text();
+    let number_delete_one = number_delete.slice(0, -1);
+    $("#number_input").text(number_delete_one);
+    defaultSet();
   });
 });
