f81815fcf01669977de201dbc00054df0abebbfc
taka0713 <tyunisu2018@gmail.com>
2022-07-03 09:43:01 +0900

    message

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 $(function () {
+  $(".phone_de").hide();
   $(".phone_button").click(function () {
+    $(".phone_de").show();
     if ($(this).data("id") !== "@") {
-      let number_input = $("#number_input").text();
-      $("#number_input").text(number_input + $(this).data("id"));
+      $("#number_input").text($("#number_input").text() + $(this).data("id"));
     } else {
-      alert($("#number_input").text() + "に電話をかけます");
+      alert($("#number_input").text() + "に電話をかけます。");
     }
+  });
+  $("#button_twelve").click(function () {
+    $("#number_input").text(" ");
+  });
+  $("#button_thirty").click(function () {
+    let sli = $("#number_input").text();
+    let dele = sli.slice(0, -1);
+    console.log(dele);
+    $("#number_input").text(dele);
   });
 });
