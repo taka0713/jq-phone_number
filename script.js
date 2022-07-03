b6d022a7d123081c42ae05887684624e52d2320b
taka0713 <tyunisu2018@gmail.com>
2022-07-04 01:12:57 +0900

    message

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 $(function () {
-  $(".phone_de").hide();
-  $(".phone_button").click(function () {
-    $(".phone_de").show();
-    if ($(this).data("id") !== "@") {
-      $("#number_input").text($("#number_input").text() + $(this).data("id"));
-    } else {
-      alert($("#number_input").text() + "に電話をかけます。");
-    }
+  let checkDisplayButton = () => {
+    $("#number_input").text() === ""
+      ? $(".phone_button_delete").hide()
+      : $(".phone_button_delete").show();
+  };
+  $(".phone_button").on("click", () => {
+    const target = $(this).data("id");
+    target !== "@"
+      ? $("#number_input").text($("#number_input").text() + target)
+      : alert($("#number_input").text() + "に電話をかけます。");
+    checkDisplayButton();
   });
-  $("#button_twelve").click(function () {
-    $("#number_input").text(" ");
-  });
-  $("#button_thirty").click(function () {
-    let sli = $("#number_input").text();
-    let dele = sli.slice(0, -1);
-    console.log(dele);
-    $("#number_input").text(dele);
+  $(".delete").on("click", () => {
+    $("#number_input").text(
+      $(this).data("id") === "all" ? "" : $("#number_input").text().slice(0, -1)
+    );
+    checkDisplayButton();
   });
 });
