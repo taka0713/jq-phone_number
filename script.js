160146953331e4d9c4165fb5e440f45d30836c50
taka0713 <tyunisu2018@gmail.com>
2022-07-11 10:13:39 +0900

    message

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 $(function () {
-  $(".phone_button").on("click", function () {
-    $("#number_input").text($("#number_input").text() + $(this).data("id"));
+  let defaultSet = () => {
     if ($("#number_input").text() === "") {
       $(".delete").hide();
     } else {
       $(".delete").show();
     }
+  };
+  $(".phone_button").on("click", function () {
+    if ($(this).data("id") !== "@") {
+      $("#number_input").text($("#number_input").text() + $(this).data("id"));
+    } else {
+      alert($("#number_input").text() + "に電話をかけます。");
+    }
+    defaultSet();
   });
   $(".delete").on("click", function () {
     if ($(this).data("id") === "all") {
@@ -13,5 +20,6 @@ $(function () {
     } else {
       $("#number_input").text($("#number_input").text().slice(0, -1));
     }
+    defaultSet();
   });
 });
